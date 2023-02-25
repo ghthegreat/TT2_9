a4a4a293faf7346a3b0f0439536d20da07f8d546
ghthegreat <56424881+ghthegreat@users.noreply.github.com>
2023-02-25 11:21:58 +0800

    updated server file

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,35 @@
-import express from "express";
-import bodyParser from "body-parser";
-import sql from "mysql";
+require('dotenv').config()
 
-import claimsRoutes from "./routes/insuranceclaims.js";
 
-const app = express();
-const PORT = 5000;
+const express = require('express')
+const mysql = require('mysql')
+const app = express()
+const PORT = 5000
+const cookieParser = require('cookie-parser')
 
-app.use(bodyParser.json());
 
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PW,
+    database: process.env.DATABASE_NAME,
+    port: 3306
+})
+
+db.connect((err) => 
+{
+    if(err)
+        console.log(err.message)
+    else
+        console.log('database connected')
+})
+
+app.use(cookieParser())
+app.set('view-engine', 'html')
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
+
+//routes
 app.use("/claims", claimsRoutes);
 app.get("/", (req, res) => res.send("Welcome to the Users API!"));
 app.all("*", (req, res) =>res.send("You've tried reaching a route that doesn't exist."));
