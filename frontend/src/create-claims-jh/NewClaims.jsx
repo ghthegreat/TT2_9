00de0c303e93574dcacd2bf6dcf72fbfd3880e6d
Jun <jun_hao_koh@hotmail.com>
2023-02-25 14:40:48 +0800

    Latest commit for date

--- frontend/src/create-claims-jh/NewClaims.jsx
+++ frontend/src/create-claims-jh/NewClaims.jsx
@@ -49,16 +49,6 @@ export function NewClaims(){
                             // onChange = {updateFormField}
                         />
                     </Form.Group>
-                    <Form.Group className="mb-1" controlId="exampleForm.ControlInput1">
-                        <Form.Label>Date of Expense</Form.Label>
-                        <Form.Control 
-                            type="date" 
-                            placeholder="DD/MM/YYYY" 
-                            name="date"
-                            // value={twoWayBind.username}
-                            // onChange = {updateFormField}
-                        />
-                    </Form.Group>
                 </div>
             </div>
 
@@ -92,6 +82,20 @@ export function NewClaims(){
                     </Form.Group>
                 </div>
             </div>
+
+            <div>
+                <Form.Group className="mb-1" controlId="exampleForm.ControlInput1">
+                    <Form.Label>Date of Expense</Form.Label>
+                        <Form.Control 
+                            type="date" 
+                            placeholder="DD/MM/YYYY" 
+                            name="date"
+                            // value={twoWayBind.username}
+                            // onChange = {updateFormField}
+                        />
+                </Form.Group>
+            </div>
+            
 
 
         </div>
