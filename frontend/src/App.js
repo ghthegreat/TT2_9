ae93e17b73e46d087d5b5bd3888c87754c95f961
Justin <justin@agmi.co>
2023-02-25 16:01:03 +0800

    edit claims page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,20 @@
-
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {HomePage} from './HomePage';
-import {ErrorPage} from './ErrorPage';
-import {NewClaims} from './create-claims-jh/NewClaims'
-
+import { HomePage } from './HomePage';
+import { ErrorPage } from './ErrorPage';
+import { NewClaims } from './create-claims-jh/NewClaims';
+import { EditClaims } from './edit-claims/EditClaims';
 
 function App() {
   return (
     <Router>
-      <Routes >
-      <Route path="/test" element={<ErrorPage></ErrorPage>}></Route>
-      <Route path="/create-claims" element = {<NewClaims/>}></Route>
+      <Routes>
+        <Route path='/test' element={<ErrorPage></ErrorPage>}></Route>
+        <Route path='/create-claims' element={<NewClaims />}></Route>
+        <Route path='/edit-claims' element={<EditClaims />}></Route>
 
         <Route index element={<HomePage></HomePage>}>
-
-        {/* add new route here
+          {/* add new route here
         <Route path="/your-path" element={<yourElement>}
          */}
         </Route>
