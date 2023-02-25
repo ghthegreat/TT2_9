938967516f484d66fcdd899384bb0c7ced7f1cfc
Alex Chua <alex.chua@zendesk.com>
2023-02-25 11:46:56 +0800

    add error page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,14 +6,16 @@ import {
   Routes,
 } from 'react-router-dom'
 import {HomePage} from './HomePage';
-
+import {ErrorPage} from './ErrorPage';
 
 
 function App() {
   return (
     <Router>
       <Routes >
+      <Route path="/test" element={<ErrorPage></ErrorPage>}></Route>
         <Route index element={<HomePage></HomePage>}>
+
         {/* add new route here
         <Route path="/your-path" element={<yourElement>}
          */}
