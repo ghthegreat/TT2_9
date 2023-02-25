ebcac7cf88d4731602e2660181e187485df88462
Jun <jun_hao_koh@hotmail.com>
2023-02-25 13:48:23 +0800

    New changes to dropdown

--- frontend/src/create-claims-jh/NewClaims.jsx
+++ frontend/src/create-claims-jh/NewClaims.jsx
@@ -4,31 +4,46 @@ import Form from 'react-bootstrap/Form'
 import './NewClaims.css'
 
 export function NewClaims(){
+
+
+
     return(
         <div className = "createPage">
             <div>
-                <Dropdown>
+                {/* <Dropdown>
                     <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
                         Insurance Policy
                     </Dropdown.Toggle>
 
                     <Dropdown.Menu variant="dark">
+                    {Array.from({ length: 4}).map((_, idx) => (
+                        <Dropdown.Item href="#/action-1" active>Car</Dropdown.Item>
+                    ))}
                         <Dropdown.Item href="#/action-1" active>Car</Dropdown.Item>
                         <Dropdown.Item>Housing</Dropdown.Item>
                         <Dropdown.Item>Personal Accident</Dropdown.Item>
                         <Dropdown.Item>Travel</Dropdown.Item>
                     </Dropdown.Menu>
-                </Dropdown>
+                </Dropdown> */}
+
+                <Form.Label>Insurance Policy</Form.Label>
+                <Form.Select aria-label="Default select example" name="variantId" /*onChange={updateFormField}*/>
+                {/* <option>-- Models --</option> */}
+                {Array.from({ length: 2 }).map((_, idx) => (
+                    <option value= "1009">
+                        Car
+                    </option>
+                ))}
+                </Form.Select>
             </div>
             
             <div className="login-section">
-                <div className = "title">LOGIN</div>
                 <div className = "login-field">
-                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                        <Form.Label>Username</Form.Label>
+                    <Form.Group className="mb-1" controlId="exampleForm.ControlInput1">
+                        <Form.Label>First name</Form.Label>
                         <Form.Control 
                             type="text" 
-                            placeholder="Enter your username" 
+                            placeholder="Enter your first name" 
                             name="username"
                             // value={twoWayBind.username}
                             // onChange = {updateFormField}
@@ -36,8 +51,6 @@ export function NewClaims(){
                     </Form.Group>
                 </div>
             </div>
-                
-
         </div>
     )
 }
