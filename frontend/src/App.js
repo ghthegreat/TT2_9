1c7158ddc8be32cb7029ec6ef5b97b2825f60766
Alex Chua <alex.chua@zendesk.com>
2023-02-25 11:44:47 +0800

    add router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import "./"
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from 'react-router-dom'
+import {HomePage} from './HomePage';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes >
+        <Route index element={<HomePage></HomePage>}>
+        {/* add new route here
+        <Route path="/your-path" element={<yourElement>}
+         */}
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
