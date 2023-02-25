00ea7aa759a36a2bf0865a143c98af7d26166c11
Zhiying15 <zhiying15@hotmail.com>
2023-02-25 10:49:39 +0800

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,6 @@ app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
 
-app.get('/express_backend', (req, res) => { //Line 9
-    res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); //Line 10
-  }); //Line 11
+app.get('/', (req, res) => {
+    res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); 
+  }); 
